d811a0fdc3434e768e99c4aef92453a227848dab
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-01-26 01:56:50 +0700

    Pengisian Nilai dengan Tugas, 12 Januari 2022

---
+++ main.c
@@ -0,0 +1,16 @@
+/* Penulis   : Fauzyah Hadirahma, email fauzyah.hadirahma@gmail.com */
+/* Deskripsi : Pengisian Nilai dengan Assignment */
+
+int main() {
+    /* KAMUS */
+    int i;
+    
+    /* ALGORITMA */
+    i = 5; /* sekarang variable i akan bernilai 5 */
+    printf ("Ini nilai i : %d \n", i);
+    
+    i = i+1; /* nilai i ditambah 1, hasilnya disimpan kembali di i */
+    printf ("Ini nilai i : %d \n", i);
+    
+    return 0;
+}
